6bf6a3856484631c58db72bbb760b4e82f15e065
Tony Burns <tony@tabolario.com>
2014-06-06 18:37:21 -0400

    roll back version so we can use stove

--- metadata.rb
+++ metadata.rb
@@ -20,7 +20,7 @@ maintainer_email 'techops@americanhonors.org'
 license 'Apache 2.0'
 description 'Installs the aws-sdk for Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.42.0'
+version '1.41.0'
 
 %w(ubuntu).each do |os|
   supports os
